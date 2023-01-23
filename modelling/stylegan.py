56babb8f3a27217f1e9d7ef764725939fd16e15d
Thien Tran <gau.nernst@yahoo.com.sg>
2023-01-23 12:01:20 +0800

    add style mixing and truncation trick for stylegan

--- modelling/stylegan.py
+++ modelling/stylegan.py
@@ -1,20 +1,83 @@
 # StyleGAN - https://arxiv.org/abs/1812.04948
 # Not implemented features
 # - Progressive growing (from Progressive GAN)
+# - R1 regularization
 #
 # Code reference:
 # https://github.com/NVlabs/stylegan
 
 import math
+from dataclasses import dataclass, replace
 from functools import partial
-from typing import List, Optional
+from typing import Optional
 
 import torch
 from torch import Tensor, nn
 
-from .base import _Act, _Norm, conv1x1, conv3x3
+from .base import _Act, conv1x1, conv3x3
 from .nvidia_ops import up_conv_blur
 from .progressive_gan import Discriminator, init_weights
+
+
+@dataclass
+class Config:
+    img_size: int = 128  # basics
+    img_depth: int = 3
+    z_dim: int = 512
+    w_dim: int = 512
+    mapping_network_depth: int = 8  # mapping network
+    w_mean_beta: float = 0.995
+    style_mixing: float = 0.9
+    truncation_psi: float = 0.7
+    truncation_cutoff: int = 8
+    smallest_map_size: int = 4  # synthesis network
+    input_depth: int = 512
+    base_depth: int = 16
+    max_depth: int = 512
+    act: _Act = partial(nn.LeakyReLU, 0.2, True)  # others
+    blur_size: int = 3
+
+
+class MappingNetwork(nn.Module):
+    def __init__(self, config: Config):
+        super().__init__()
+        self.mlp = nn.Sequential()
+        for i in range(config.mapping_network_depth):
+            self.mlp.append(nn.Linear(config.z_dim if i == 0 else config.w_dim, config.w_dim))
+            self.mlp.append(config.act())
+        self.register_buffer("w_mean", torch.zeros(config.w_dim))
+        self.w_mean: Tensor
+        self.w_mean_beta = config.w_mean_beta
+        self.style_mixing = config.style_mixing
+        self.truncation_psi = config.truncation_psi
+        self.truncation_cutoff = config.truncation_cutoff
+        self.n_layers = int(math.log2(config.img_size // config.smallest_map_size) + 1) * 2
+
+    def forward(self, z_embs: Tensor):
+        w_embs = self.mlp(z_embs)
+
+        if self.training:
+            self.w_mean.lerp_(w_embs.detach().mean(0), 1 - self.w_mean_beta)
+            w_embs = w_embs.unsqueeze(1).expand(-1, self.n_layers, -1)
+
+            if self.style_mixing > 0 and torch.rand([]) < self.style_mixing:
+                w_embs2 = self.mlp(torch.randn_like(z_embs))
+                w_embs2 = w_embs2.unsqueeze(1).expand(-1, self.n_layers, -1)
+
+                cutfoff = torch.arange(self.n_layers) < torch.randint(1, self.n_layers, (self.n_layers,))
+                cutfoff = cutfoff.view(1, self.n_layers, 1).to(w_embs.device)
+                w_embs = torch.where(cutfoff, w_embs, w_embs2)
+
+        else:
+            w_embs = w_embs.unsqueeze(1).expand(-1, self.n_layers, -1)
+
+        if self.truncation_cutoff > 0:
+            w_embs_trunc = self.w_mean.lerp(w_embs, self.truncation_psi)
+            cutoff = torch.arange(self.n_layers) < self.truncation_cutoff
+            cutoff = cutoff.view(1, self.n_layers, 1).to(w_embs.device)
+            w_embs = torch.where(cutoff, w_embs_trunc, w_embs)
+
+        return w_embs
 
 
 class GeneratorBlock(nn.Module):
@@ -22,25 +85,22 @@ class GeneratorBlock(nn.Module):
         self,
         in_dim: int,
         out_dim: int,
-        w_dim: int,
+        config: Config,
         first_block: bool = False,
         upsample: bool = False,
-        norm: _Norm = nn.InstanceNorm2d,
-        act: _Act = partial(nn.LeakyReLU, 0.2, True),
-        blur_size: int = 3,
     ):
         super().__init__()
         if first_block:
             self.conv = nn.Identity()
         elif upsample:
-            self.conv = up_conv_blur(in_dim, out_dim, 3, blur_size=blur_size)
+            self.conv = up_conv_blur(in_dim, out_dim, 3, blur_size=config.blur_size)
         else:
             self.conv = conv3x3(in_dim, out_dim)
         self.noise_weight = nn.Parameter(torch.zeros(1, out_dim, 1, 1))  # B in paper
-        self.act = act()
-        self.norm = norm(out_dim)
-        self.style_weight = nn.Linear(w_dim, out_dim)  # A in paper
-        self.style_bias = nn.Linear(w_dim, out_dim)
+        self.act = config.act()
+        self.norm = nn.InstanceNorm2d(out_dim)
+        self.style_weight = nn.Linear(config.w_dim, out_dim)  # A in paper
+        self.style_bias = nn.Linear(config.w_dim, out_dim)
 
     def forward(self, imgs: Tensor, w_embs: Tensor, noise: Optional[Tensor] = None):
         imgs = self.conv(imgs)
@@ -55,48 +115,36 @@ class GeneratorBlock(nn.Module):
 
 
 class Generator(nn.Module):
-    def __init__(
-        self,
-        img_size: int,
-        img_depth: int = 3,
-        z_dim: int = 512,
-        w_dim: int = 512,
-        mapping_network_depth: int = 8,
-        input_depth: int = 512,
-        smallest_map_size: int = 4,
-        base_depth: int = 16,
-        max_depth: int = 512,
-        act: _Act = partial(nn.LeakyReLU, 0.2, True),
-        blur_size: int = 3,
-    ):
-        assert img_size > 4 and math.log2(img_size).is_integer()
+    def __init__(self, config: Optional[Config] = None, **kwargs):
+        config = config or Config()
+        config = replace(config, **kwargs)
+        assert config.img_size > 4 and math.log2(config.img_size).is_integer()
         super().__init__()
-        self.mapping_network = nn.Sequential()
-        for i in range(mapping_network_depth):
-            self.mapping_network.append(nn.Linear(z_dim if i == 0 else w_dim, w_dim))
-            self.mapping_network.append(act())
+        self.mapping_network = MappingNetwork(config)
 
-        self.learned_input = nn.Parameter(torch.empty(1, input_depth, smallest_map_size, smallest_map_size))
+        map_size = config.smallest_map_size
+        in_depth = config.input_depth
+        self.learned_input = nn.Parameter(torch.empty(1, in_depth, map_size, map_size))
 
-        block = partial(GeneratorBlock, w_dim=w_dim, act=act, blur_size=blur_size)
-        depth = base_depth * img_size // smallest_map_size
-        out_depth = min(depth, max_depth)
+        block = partial(GeneratorBlock, config=config)
+        depth = config.base_depth * config.img_size // map_size
+        out_depth = min(depth, config.max_depth)
 
         self.layers = nn.ModuleList()
-        self.layers.append(block(input_depth, input_depth, first_block=True))
-        self.layers.append(block(input_depth, out_depth))
-        input_depth = out_depth
+        self.layers.append(block(in_depth, in_depth, first_block=True))
+        self.layers.append(block(in_depth, out_depth))
+        in_depth = out_depth
         depth //= 2
 
-        while smallest_map_size < img_size:
-            out_depth = min(depth, max_depth)
-            self.layers.append(block(input_depth, out_depth, upsample=True))
+        while map_size < config.img_size:
+            out_depth = min(depth, config.max_depth)
+            self.layers.append(block(in_depth, out_depth, upsample=True))
             self.layers.append(block(out_depth, out_depth))
-            input_depth = out_depth
+            in_depth = out_depth
             depth //= 2
-            smallest_map_size *= 2
+            map_size *= 2
 
-        self.out_conv = conv1x1(input_depth, img_depth)
+        self.out_conv = conv1x1(in_depth, config.img_depth)
 
         self.reset_parameters()
 
@@ -105,8 +153,8 @@ class Generator(nn.Module):
         self.layers.apply(init_weights)
 
     def forward(self, z_embs: Tensor):
-        w_embs = self.mapping_network(z_embs)
-        x = self.learned_input.repeat(z_embs.size(0), 1, 1, 1)
-        for layer in self.layers:
-            x = layer(x, w_embs)
+        w_embs = self.mapping_network(z_embs)  # (batch_size, n_layers, w_dim)
+        x = self.learned_input.expand(z_embs.size(0), -1, -1, -1)
+        for i, layer in enumerate(self.layers):
+            x = layer(x, w_embs[:, i])
         return self.out_conv(x)
