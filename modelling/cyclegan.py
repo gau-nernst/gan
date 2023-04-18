4ef1533ef080b4445042d1cd39db6a78cee22c0a
Thien Tran <gau.nernst@yahoo.com.sg>
2023-04-18 23:56:27 +0800

    add UnetGenerator for pix2pix

--- modelling/cyclegan.py
+++ modelling/cyclegan.py
@@ -1,17 +1,23 @@
+# Code reference:
+# https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix
+
 from functools import partial
 
+import torch
 from torch import Tensor, nn
 
 from .base import _Act, _Norm, conv3x3, conv_norm_act
 from .dcgan import init_weights
+from .pix2pix import PatchGAN
 
 
 # almost identical to torchvision.models.resnet.BasicBlock
 class ResNetBlock(nn.Module):
-    def __init__(self, in_channels: int, out_channels: int, norm: _Norm, act: _Act):
+    def __init__(self, in_channels: int, out_channels: int, dropout: float, norm: _Norm, act: _Act):
         super().__init__()
         self.main = nn.Sequential(
             conv_norm_act(in_channels, out_channels, conv3x3, norm, act),
+            nn.Dropout(dropout),
             conv_norm_act(out_channels, out_channels, conv3x3, norm, nn.Identity),
         )
 
@@ -27,6 +33,7 @@ class ResNetGenerator(nn.Sequential):
         base_channels: int = 64,
         n_blocks: int = 9,
         downsample: int = 2,
+        dropout: float = 0.5,
         norm: _Norm = nn.InstanceNorm2d,
         act: _Act = partial(nn.ReLU, inplace=True),
     ):
@@ -43,7 +50,7 @@ class ResNetGenerator(nn.Sequential):
 
         self.resnet_blocks = nn.Sequential()
         for _ in range(n_blocks):
-            self.resnet_blocks.append(ResNetBlock(base_channels, base_channels, norm, act))
+            self.resnet_blocks.append(ResNetBlock(base_channels, base_channels, dropout, norm, act))
 
         self.up_blocks = nn.Sequential()
         for _ in range(downsample):
@@ -56,3 +63,21 @@ class ResNetGenerator(nn.Sequential):
 
     def reset_parameters(self):
         self.apply(init_weights)
+
+
+class CycleGANGenerator(nn.Module):
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.model = ResNetGenerator(**kwargs)
+
+    def forward(self, z_embs: Tensor, ys: Tensor):
+        return self.model(ys)
+
+
+class CycleGANDiscriminator(nn.Module):
+    def __init__(self, img_channels: int, y_channels: int, **kwargs):
+        super().__init__()
+        self.model = PatchGAN(img_channels + y_channels, **kwargs)
+
+    def forward(self, imgs: Tensor, ys: Tensor):
+        return self.model(torch.cat([imgs, ys], dim=1))
--- modelling/pix2pix.py
+++ modelling/pix2pix.py
@@ -1,8 +1,12 @@
+# Code reference:
+# https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix
+
 from functools import partial
 
-from torch import nn
+import torch
+from torch import Tensor, nn
 
-from .base import _Act, _Norm, conv_norm_act, leaky_relu
+from .base import _Act, _Norm, conv_norm_act, leaky_relu, relu
 
 
 class PatchGAN(nn.Sequential):
@@ -15,15 +19,72 @@ class PatchGAN(nn.Sequential):
         act: _Act = leaky_relu,
     ):
         super().__init__()
-        conv = partial(nn.Conv2d, kernel_size=4, padding=1)
+        conv4x4 = partial(nn.Conv2d, kernel_size=4, padding=1)
 
-        def get_out_c(idx):
+        def get_out_c(idx: int):
             return base_channels * 2 ** min(idx, 3)
 
-        self.append(nn.Sequential(conv(in_channels, base_channels, stride=2), act()))
+        self.append(nn.Sequential(conv4x4(in_channels, base_channels, stride=2), act()))
 
         for i in range(1, n_layers):
-            self.append(conv_norm_act(get_out_c(i - 1), get_out_c(i), conv, norm, act, stride=2))
+            self.append(conv_norm_act(get_out_c(i - 1), get_out_c(i), conv4x4, norm, act, stride=2))
 
-        self.append(conv_norm_act(get_out_c(n_layers - 1), get_out_c(n_layers), conv, norm, act))
-        self.append(conv(get_out_c(n_layers), 1))
+        self.append(conv_norm_act(get_out_c(n_layers - 1), get_out_c(n_layers), conv4x4, norm, act))
+        self.append(conv4x4(get_out_c(n_layers), 1))
+
+
+class UnetGenerator(nn.Module):
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        n_stages: int,
+        base_channels: int = 64,
+        norm: _Norm = nn.InstanceNorm2d,
+        down_act: _Act = leaky_relu,
+        up_act: _Act = relu,
+    ):
+        super().__init__()
+        down_conv = partial(nn.Conv2d, kernel_size=4, stride=2, padding=1)
+        up_conv = partial(nn.ConvTranspose2d, kernel_size=4, stride=2, padding=1)
+
+        def get_out_c(idx: int):
+            return base_channels * 2 ** min(idx, 3)
+
+        def act_conv_norm(in_c: int, out_c: int, down: bool):
+            return nn.Sequential(
+                (down_act if down else up_act)(),
+                (down_conv if down else up_conv)(in_c, out_c, bias=False),
+                norm(out_c),
+            )
+
+        self.downs = nn.ModuleList()
+        self.downs.append(down_conv(in_channels, base_channels))
+        for i in range(1, n_stages - 1):
+            self.downs.append(act_conv_norm(get_out_c(i - 1), get_out_c(i), True))
+
+        self.last_stage = nn.Sequential(
+            down_act(),
+            down_conv(get_out_c(n_stages - 2), get_out_c(n_stages - 1)),
+            up_act(),
+            up_conv(get_out_c(n_stages - 1), get_out_c(n_stages - 2), bias=False),
+            norm(get_out_c(n_stages - 2)),
+        )
+
+        self.ups = nn.ModuleList()
+        for i in range(n_stages - 2, 0, -1):
+            self.ups.append(act_conv_norm(get_out_c(i) * 2, get_out_c(i - 1), False))
+        self.ups.append(nn.Sequential(up_act(), up_conv(get_out_c(0) * 2, out_channels), nn.Tanh()))
+
+    def forward(self, imgs: Tensor):
+        fmaps = []
+        for down in self.downs:
+            imgs = down(imgs)
+            fmaps.append(imgs)
+
+        out = self.last_stage(imgs)
+
+        for up in self.ups:
+            out = up(torch.cat([out, fmaps.pop()], 1))
+
+        return out
