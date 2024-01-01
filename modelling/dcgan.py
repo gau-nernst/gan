bdffbd5543f1c5e27fd3a897e72224b9775b5551
Thien Tran <gau.nernst@yahoo.com.sg>
2024-01-01 15:45:36 +0800

    simplify DCGAN

--- modelling/dcgan.py
+++ modelling/dcgan.py
@@ -6,81 +6,65 @@
 # https://github.com/soumith/dcgan.torch/
 # https://github.com/martinarjovsky/WassersteinGAN/
 
-from functools import partial
+import math
 
-from torch import Tensor, nn
-
-from .base import _Act, _Norm, conv_norm_act, leaky_relu, relu
+from torch import nn
 
 
-class DCGANDiscriminator(nn.Module):
-    def __init__(
-        self,
-        img_size: int = 64,
-        img_channels: int = 3,
-        init_map_size: int = 4,
-        min_channels: int = 64,
-        norm: _Norm = partial(nn.BatchNorm2d, track_running_stats=False),
-        act: _Act = leaky_relu,
-    ):
+class DCGANDiscriminator(nn.Sequential):
+    def __init__(self, img_channels: int = 3, img_size: int = 64, base_dim: int = 64) -> None:
         super().__init__()
-        self.layers = nn.Sequential()
+        in_ch = img_channels
 
-        # add strided conv until image size = 4
-        conv = partial(nn.Conv2d, kernel_size=4, stride=2, padding=1)
-        while img_size > init_map_size:
-            self.layers.append(conv_norm_act(img_channels, min_channels, conv, norm, act))
-            img_channels = min_channels
-            min_channels *= 2
-            img_size //= 2
+        # strided conv until 4x4
+        for i in range(int(math.log2(img_size / 4))):
+            out_ch = base_dim * 2**i
+            new_layers = [
+                nn.Conv2d(in_ch, out_ch, 4, 2, 1),
+                nn.BatchNorm2d(out_ch, track_running_stats=False),
+                nn.LeakyReLU(0.2, inplace=True),
+            ]
+            self.extend(new_layers)
+            in_ch = out_ch
 
         # flatten and matmul
-        self.layers.append(nn.Conv2d(img_channels, 1, init_map_size))
-
+        self.extend([nn.Conv2d(in_ch, 1, 4), nn.Flatten(0)])
         self.reset_parameters()
 
     def reset_parameters(self):
         self.apply(init_weights)
 
-    def forward(self, imgs: Tensor):
-        return self.layers(imgs).view(-1)
 
-
-class DCGANGenerator(nn.Module):
-    def __init__(
-        self,
-        img_size: int = 64,
-        img_channels: int = 3,
-        z_dim: int = 128,
-        init_map_size: int = 4,
-        min_channels: int = 64,
-        norm: _Norm = partial(nn.BatchNorm2d, track_running_stats=False),
-        act: _Act = relu,
-    ):
-        super().__init__()
-        self.layers = nn.Sequential()
+class DCGANGenerator(nn.Sequential):
+    def __init__(self, img_channels: int = 3, img_size: int = 64, base_dim: int = 64, z_dim: int = 128) -> None:
+        out_ch = base_dim * img_size // 8
 
         # matmul and reshape to 4x4
-        channels = min_channels * img_size // 2 // init_map_size
-        self.layers.append(conv_norm_act(z_dim, channels, nn.ConvTranspose2d, norm, act, kernel_size=init_map_size))
+        super().__init__(
+            nn.Unflatten(-1, (z_dim, 1, 1)),
+            nn.ConvTranspose2d(z_dim, out_ch, 4),
+            nn.BatchNorm2d(out_ch, track_running_stats=False),
+            nn.ReLU(inplace=True),
+        )
+        in_ch = out_ch
 
         # conv transpose until reaching image size / 2
-        conv = partial(nn.ConvTranspose2d, kernel_size=4, stride=2, padding=1)
-        while init_map_size < img_size // 2:
-            self.layers.append(conv_norm_act(channels, channels // 2, conv, norm, act))
-            channels //= 2
-            init_map_size *= 2
+        for _ in range(int(math.log2(img_size / 4)) - 1):
+            out_ch = in_ch // 2
+            new_layers = [
+                nn.ConvTranspose2d(in_ch, out_ch, 4, 2, 1),
+                nn.BatchNorm2d(out_ch, track_running_stats=False),
+                nn.ReLU(inplace=True),
+            ]
+            self.extend(new_layers)
+            in_ch = out_ch
 
-        # last layer use tanh activation
-        self.layers.extend([conv(channels, img_channels), nn.Tanh()])
-
+        # last layer: no bn and use tanh activation
+        self.extend([nn.ConvTranspose2d(in_ch, img_channels, 4, 2, 1), nn.Tanh()])
         self.reset_parameters()
 
     def reset_parameters(self):
         self.apply(init_weights)
-
-    def forward(self, z_embs: Tensor):
-        return self.layers(z_embs[:, :, None, None])
 
 
 def init_weights(module: nn.Module):
