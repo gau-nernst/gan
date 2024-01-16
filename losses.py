fix MinibatchStdDev mixed precision

losses.py
@@ -91,7 +91,7 @@ def wgan_gp_regularizer(disc: nn.Module, reals: Tensor, fakes: Tensor, d_reals: 
     d_inter = disc(inter)
 
     with torch.autocast(reals.device.type, enabled=False):
-        (d_grad,) = torch.autograd.grad(d_inter.sum(), inter, create_graph=True)
+        (d_grad,) = torch.autograd.grad(d_inter.float().sum(), inter, create_graph=True)
 
     d_grad_norm = torch.linalg.vector_norm(d_grad.flatten(1), dim=1)
     return (d_grad_norm - 1).square().mean() * 10
@@ -100,7 +100,7 @@ def wgan_gp_regularizer(disc: nn.Module, reals: Tensor, fakes: Tensor, d_reals: 
 # https://arxiv.org/abs/1801.04406
 def r1_regularizer(disc: nn.Module, reals: Tensor, fakes: Tensor, d_reals: Tensor) -> Tensor:
     with torch.autocast(reals.device.type, enabled=False):
-        (d_grad,) = torch.autograd.grad(d_reals.sum(), reals, create_graph=True)
+        (d_grad,) = torch.autograd.grad(d_reals.float().sum(), reals, create_graph=True)
 
     d_grad_norm2 = d_grad.square().sum() / d_grad.shape[0]
     return d_grad_norm2 * 5

modelling/progressive_gan.py
@@ -29,12 +29,17 @@ class MinibatchStdDev(nn.Module):
         self.group_size = group_size
 
     def forward(self, imgs: Tensor) -> Tensor:
-        imgs = imgs.float()  # always compute in fp32
         N, C, H, W = imgs.shape
-        std = imgs.view(self.group_size, -1, C, H, W).std(0, unbiased=False)
+        std = self.std(imgs.view(self.group_size, -1, C, H, W))
         std = std.mean([1, 2, 3], keepdim=True)
         std = std.repeat(self.group_size, 1, 1, 1).expand(N, 1, H, W)
         return torch.cat([imgs, std], dim=1)
+
+    # torch.std() might return 0. when doing gradient penalty, this will result in division by zero in backward pass.
+    # thus, we add eps=1e-6 before performing square root.
+    @staticmethod
+    def std(x: Tensor) -> Tensor:
+        return (x - x.mean(0)).square().sum(0).div(x.shape[0]).add(1e-6).sqrt()
 
 
 # this is identical SA-GAN, except activation function.