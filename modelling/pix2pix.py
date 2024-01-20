773d0eb4cd7607c9c7a5fc34efa2a0d58819ffd5
Thien Tran <gau.nernst@yahoo.com.sg>
2024-01-20 22:38:55 +0800

    fix pix2pix

--- modelling/pix2pix.py
+++ modelling/pix2pix.py
@@ -17,16 +17,21 @@ class PatchGan(nn.Sequential):
 
         for i in range(1, depth):
             out_ch = base_dim * 2 ** min(i, 3)
-            self.append(nn.Conv2d(in_ch, out_ch, 4, 2, 1, bias=False))
+            self.append(nn.Conv2d(in_ch, out_ch, 4, 2, 1))
             self.append(nn.InstanceNorm2d(out_ch))
             self.append(nn.LeakyReLU(0.2, inplace=True))
             in_ch = out_ch
 
         out_ch = base_dim * 2 ** min(depth, 3)
-        self.append(nn.Conv2d(in_ch, out_ch, 4, 1, 1, bias=False))
+        self.append(nn.Conv2d(in_ch, out_ch, 4, 1, 1))
         self.append(nn.InstanceNorm2d(out_ch))
         self.append(nn.LeakyReLU(0.2, inplace=True))
         self.append(nn.Conv2d(out_ch, 1, 4, 1, 1))
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        self.apply(init_weights)
 
     def forward(self, imgs_A: Tensor, imgs_B: Tensor):
         return super().forward(torch.cat([imgs_A, imgs_B], dim=1))
@@ -35,63 +40,64 @@ class PatchGan(nn.Sequential):
 class UnetGenerator(nn.Module):
     def __init__(self, A_channels: int = 3, B_channels: int = 3, depth: int = 7, base_dim: int = 64):
         super().__init__()
-        self.downs = nn.ModuleList()
-        self.downs.append(nn.Conv2d(A_channels, base_dim, 4, 2, 1))
+        self.down_blocks = nn.ModuleList()
+        self.down_blocks.append(nn.Conv2d(A_channels, base_dim, 4, 2, 1))
         in_ch = base_dim
         for i in range(1, depth - 1):
             out_ch = base_dim * 2 ** min(i, 3)
             block = nn.Sequential(
                 nn.LeakyReLU(0.2, inplace=True),
-                nn.Conv2d(in_ch, out_ch, 4, 2, 1, bias=False),
+                nn.Conv2d(in_ch, out_ch, 4, 2, 1),
                 nn.InstanceNorm2d(out_ch),
             )
-            self.downs.append(block)
+            self.down_blocks.append(block)
             in_ch = out_ch
 
         out_ch = base_dim * 2 ** min(depth - 1, 3)
-        self.last_stage = nn.Sequential(
+        self.inner_most = nn.Sequential(
             nn.LeakyReLU(0.2, inplace=True),
             nn.Conv2d(in_ch, out_ch, 4, 2, 1),
             nn.ReLU(inplace=True),
-            nn.ConvTranspose2d(out_ch, in_ch, 4, 2, 1, bias=False),
+            nn.ConvTranspose2d(out_ch, in_ch, 4, 2, 1),
             nn.InstanceNorm2d(in_ch),
         )
 
-        self.ups = nn.ModuleList()
-        for i in range(depth - 2, 0, -1):
+        self.up_blocks = nn.ModuleList()
+        for i in range(depth - 3, -1, -1):
             out_ch = base_dim * 2 ** min(i, 3)
             block = nn.Sequential(
                 nn.ReLU(inplace=True),
-                nn.ConvTranspose2d(in_ch, out_ch, 4, 2, 1, bias=False),
+                nn.ConvTranspose2d(in_ch * 2, out_ch, 4, 2, 1),
                 nn.InstanceNorm2d(out_ch),
             )
+            self.up_blocks.append(block)
             in_ch = out_ch
-        self.ups.append(nn.Sequential(nn.ReLU(inplace=True), nn.ConvTranspose2d(in_ch, B_channels), nn.Tanh()))
+        self.up_blocks.append(
+            nn.Sequential(
+                nn.ReLU(inplace=True),
+                nn.ConvTranspose2d(in_ch * 2, B_channels, 4, 2, 1),
+                nn.Tanh(),
+            )
+        )
 
         self.reset_parameters()
 
     def reset_parameters(self):
         self.apply(init_weights)
 
-    def forward(self, imgs: Tensor):
-        fmaps = []
-        for down in self.downs:
-            imgs = down(imgs)
-            fmaps.append(imgs)
+    def forward(self, x: Tensor):
+        fmaps = [x]
+        for down in self.down_blocks:
+            fmaps.append(down(fmaps[-1]))
 
-        out = self.last_stage(imgs)
+        out = self.inner_most(fmaps[-1])
 
-        for up in self.ups:
+        for up in self.up_blocks:
             out = up(torch.cat([out, fmaps.pop()], 1))
-
         return out
 
 
 def init_weights(module: nn.Module):
     if isinstance(module, (nn.modules.conv._ConvNd, nn.modules.conv._ConvTransposeNd)):
         nn.init.normal_(module.weight, 0, 0.02)
-        if module.bias is not None:
-            nn.init.zeros_(module.bias)
-    elif isinstance(module, nn.modules.InstanceNorm2d):
-        nn.init.normal_(module.weight, 1, 0.02)
         nn.init.zeros_(module.bias)
